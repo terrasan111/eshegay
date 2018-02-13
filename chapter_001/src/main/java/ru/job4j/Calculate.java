ab145a7d737100af14317802b61fecb26bb9bb90
Evgenii Shegay <terrasan34@gmail.com>
2018-02-13 13:11:55 +0900

    Подключил плагин Maven , checkstyle ,отредактировал файл gitignore , Создал класс калькулятор и тест к нему , подключил плагин JUnit and плагин JaCoCo

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+/**
+ * Calculate class.
+ *@author Evgeniy Shegay (terrasan30@gmail.com)
+ *since 15.11.2017
+ */
+
+
+public class Calculate {
+
+   public static void main(String[] args) {
+       System.out.println("Hello World.");
+   }
+   
+   /**
+ * Method echo.
+ * @param name Your name.
+ * @return Echo plus your name.
+ */
+ 
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+}
