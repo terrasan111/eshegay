fb48ebb5abf06dca90c2e55a0197b4cc3b50eac7
Evgenii Shegay <terrasan34@gmail.com>
2018-02-14 14:26:34 +0900

    Конвертация двумерного массива в ArrayList и наоборот  [10035]

---
+++ chapter_003/src/main/java/ru/job4j/collections/Convertation.java
@@ -0,0 +1,72 @@
+package ru.job4j.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Convertation {
+
+
+
+
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+       for (int i = 0; i < array.length; i++) {
+           for (int a : array[i]) {
+               list.add(a);
+           }
+       }
+       return list;
+    }
+
+
+    public int[][] toArray(List<Integer> test, int rows) {
+
+       int column = rows;
+       int[][] result = new int[rows][column];
+       int a = test.size();
+       int count = 0;
+       if (Math.ceil((double) a / (double) rows) == 3.0) {
+           for (int i = 0; i < rows; i++) {
+
+                   for (int j = 0; j < column; j++) {
+                               result[i][j] = count(test, rows, a, count);
+                               if (a >= 0) {
+                                   a--;
+                               }
+                               count++;
+                   }
+               }
+           }
+
+
+        return result;
+    }
+
+    public int count(List<Integer> list, int rows, int a, int count) {
+      int result = 0;
+      if (list.size() % rows == 0) {
+       result = list.get(count);
+      count++;
+      } else if (list.size() % rows == 1) {
+
+          if (a > 0) {
+              result = list.get(count);
+          } else {
+              result = 0;
+          }
+      }
+      return result;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] tmp : list) {
+            for (int i : tmp) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+}
+
---
+++ chapter_003/src/test/java/ru/job4j/collections/ConvertationTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.collections;
+
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertationTest {
+
+    @Test
+    public void convertArrayInList() {
+        int[][] test = new int[][]  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        List<Integer> result = new ArrayList<>();
+        Convertation convertation = new Convertation();
+        result = convertation.toList(test);
+        List<Integer> expected = new ArrayList<>(9);
+        int j = 1;
+        for (int i = 0; i < 9; i++) {
+            expected.add(j);
+            j++;
+        }
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void convertListInArray() {
+        List<Integer> test = new ArrayList<>();
+        int j = 1;
+        for (int i = 0; i < 7; i++) {
+            test.add(j);
+            j++;
+        }
+        Convertation convertation = new Convertation();
+        int[][] tmp = convertation.toArray(test, 3);
+        int[][] expected = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
+        assertThat(tmp, is(expected));
+    }
+
+  /*  @Test
+    public void test3() {
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6, 7};
+        List<Integer> expect = new ArrayList<>();
+        int j = 1;
+        for (int i = 0; i < 7; i++) {
+            expect.add(j);
+            j++;
+        }
+        Convertation convertation = new Convertation();
+        List<Integer> test = new ArrayList<>();
+         test = convertation.convert(Arrays.asList(a, b));
+         assertThat(test, is(expect));
+    }
+
+    @Test
+    public void test4() {
+        User user1 = new User("Jack", "New York", 11);
+        User user2 = new User("John", "Mexico", 122);
+        User user3 = new User("Stefan", "Mistic Falls", 13);
+        User user4 = new User("Daemon", "Mistic Falls", 33);
+        List<User> user = new ArrayList<>();
+        user.add(user1);
+        user.add(user2);
+        user.add(user3);
+        user.add(user4);
+        UserConvert userConvert = new UserConvert();
+        HashMap<Integer, User> result = new HashMap<>();
+        result = userConvert.process(user);
+        HashMap<Integer, User> expect = new HashMap<>();
+        expect.put(11, user1);
+        expect.put(122, user2);
+        expect.put(13, user3);
+        expect.put(33, user4);
+        assertThat(result, is(expect));
+    }
+*/
+}
