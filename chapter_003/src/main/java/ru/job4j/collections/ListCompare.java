651a78b733050ae65e82409535f73869b42edbd6
Evgenii Shegay <terrasan34@gmail.com>
2018-02-15 13:32:37 +0900

    Компаратор для списков [35008]

---
+++ chapter_003/src/main/java/ru/job4j/collections/ListCompare.java
@@ -0,0 +1,34 @@
+package ru.job4j.collections;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ListCompare implements Comparator<List<Integer>>  {
+
+    @Override
+    public int compare(List<Integer> left, List<Integer> right) {
+        int result = 0;
+        int i;
+        int count = 0;
+        if (left.size() == right.size()) {
+            for (int a = 0; a < left.size(); a++) {
+                i = left.get(a);
+                    result = Integer.compare(i, simpleLoop(right, count));
+                    count++;
+                }
+            }
+         if (left.size() < right.size()) {
+           result = -1;
+         }
+        return result;
+    }
+
+    public int simpleLoop(List<Integer> right, int count) {
+        int j = 0;
+        while (count < right.size()) {
+            j = right.get(count);
+            break;
+        }
+        return j;
+    }
+}
