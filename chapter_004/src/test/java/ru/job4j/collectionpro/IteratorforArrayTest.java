109c4874125b3c3f811dddb55185e823a67e9a33
Evgenii Shegay <terrasan34@gmail.com>
2018-02-22 20:03:39 +0900

    Итератор для двумерного массива int  [9539]

---
+++ chapter_004/src/test/java/ru/job4j/collectionpro/IteratorforArrayTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.collectionpro;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class IteratorforArrayTest {
+
+
+    @Test
+    public void hasNextNextSequentialInvocation() {
+
+
+        IteratorforArray it = new IteratorforArray();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+
+
+        IteratorforArray it = new IteratorforArray();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+
+
+        IteratorforArray it = new IteratorforArray();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+    }
+
+
+}
